9ee20b067e5bb15416cec7645db4731cce53d382
exdeleter <exdeleter@gmail.com>
2021-11-07 01:03:52 +0300

    fixed program

--- k_means/k_means.cpp
+++ k_means/k_means.cpp
@@ -1,6 +1,8 @@
 #include<iostream>
 #include<conio.h>
 #include <vector>
+#include <fstream>
+#include<string>  
 using namespace std;
 
 void onemeasure()
@@ -116,16 +118,15 @@ void Random_Value_Vector(vector<Point>& points,int  amount)
         Point temp;
         temp.x = (rand() % 1001) / 1000.0;
         temp.y = (rand() % 1001) / 1000.0;
-        points[i] =temp;
+        points[i] = temp;
     }
 }
-void Out_Point_Vector(vector<Point>& points, int size)
+void Out_Point_Vector(vector<Point> points, int size)
 {
     std::cout << endl;
     for (int i = 0; i < size; i++)
     {
         std::cout << i << ":" << "(" << points[i].x << "," << points[i].y << ")" << endl;
-
     }
 }
 void Copy(vector<Point> FromV, vector<Point>& ToV)
@@ -151,12 +152,58 @@ void Print_clusters_vectors(vector<vector<Point>> clusters, int count_clusters)
         cout << endl;
     }
 }
-bool flagequal_vector(vector<Point> oldmean, vector<Point> mean)
+bool flag_equal_vector(vector<Point> oldmean, vector<Point> mean)
 {
     for (int i = 0; i < oldmean.size(); i++)
-        if (oldmean[i].x - mean[i].x <= 0.0001 && oldmean[i].y - mean[i].y <= 0.0001)
-            return false;
-    return true;
+        if (abs(mean[i].x - oldmean[i].x) >= 0.0001 || abs(mean[i].y - oldmean[i].y) >= 0.0001)
+            return true;
+    return false;
+}
+void Out_Point_Vector_File(vector<Point> points, int size)
+{
+    std::ofstream out("Output\\points.txt");
+    if (out.is_open())
+    {
+        for (int i = 0; i < size; i++)
+        {
+            out << points[i].x << " " << points[i].y << endl;
+        }
+    }
+    out.close();
+
+}
+void Out_Point_Clusters_File(vector<vector<Point>> points, int count)
+{
+    for (int i = 0; i < count; i++)
+    {
+        string name = "Output\\Clusters\\" + to_string(i) + ".txt"; // create name for .txt file
+
+        std::ofstream file(name);
+        if (file.is_open())
+            for (int j = 0; j < points[i].size(); j++)
+                file << points[i][j].x << " " << points[i][j].y << endl;
+        file.close();
+    }
+   
+
+}
+void Mean_between_points(vector<vector<Point>> clusters, vector<Point>& mean_value, int count_clusters)
+{
+
+    for (int i = 0; i < count_clusters; i++)
+    {
+        double sumX=0;
+        double sumY = 0;
+        cout << "\nCluster " << i << ": " << mean_value[i].x << "," << mean_value[i].y << endl;
+        for (int j = 0; j < clusters[i].size(); j++)
+        {
+            sumX += clusters[i][j].x;
+            sumY += clusters[i][j].y;
+        }
+        mean_value[i].x = sumX / clusters[i].size();
+        mean_value[i].y = sumY / clusters[i].size();
+        cout  << mean_value[i].x << "," << mean_value[i].y << endl;
+    }
 }
 void k_means_2x_with_vector()
 {
@@ -164,31 +211,37 @@ void k_means_2x_with_vector()
     int amount;
     cin >> amount;
     std::vector<Point> points(amount); // create point for clusterization
-    Random_Value_Vector(points, amount); // fill it random values
+    Random_Value_Vector(points, amount); // fill it random values. 
+    //in future we can change random value - reading value from file
     Out_Point_Vector(points, amount); // out it
+
     cout << "\nEnter count of clusters: ";
     int count_clasters;
     cin >> count_clasters;
-    std::vector <vector<Point>> clusters(count_clasters); // create cluster's that need for write it's points
+
+    std::vector <vector<Point>> clusters(count_clasters);// create cluster's that need for write it's points
     vector<Point> mean_value(count_clasters); 
     Random_Value_Vector(mean_value, count_clasters); // fill it
     cout << "\nCentral mean: ";
     Out_Point_Vector(mean_value, count_clasters); // out it
     vector<Point> old_mean_value(count_clasters); // create old mean_cluster for compare. need us later
+    
+
     do
     {
+        clusters.clear();
+        clusters.resize(count_clasters);
         vector<double> distance(count_clasters);
         Copy(mean_value, old_mean_value);
         for (int i = 0; i < amount; i++) //loop for measure distance between points and clusters
         {
             
             for (int j = 0; j < count_clasters; j++) // loop for measure distance between current point and mean cluster
-            {
-
                 distance[j] = Measure_distance(points[i],mean_value[j]);
-            }
+
             int temp = 0; // temp for undertanding between which cluster and point min distance
             double min = distance[0];
+
             for (int j = 0; j < count_clasters; j++) // find min between points
             {
                 if (min > distance[j])
@@ -197,12 +250,17 @@ void k_means_2x_with_vector()
                     temp = j;
                 }
             }
+
             clusters[temp].push_back(points[i]); // add point in clusters
-            Print_clusters_vectors(clusters, count_clasters); //out 
-
+            
         }
+        Print_clusters_vectors(clusters, count_clasters); // out clusters in console
+        Mean_between_points(clusters, mean_value, count_clasters);
 
-    } while (flagequal_vector(mean_value, old_mean_value));
+    } while (flag_equal_vector(mean_value, old_mean_value)); // check how change distance between center of clusters
+    
+    Out_Point_Vector_File(points, amount); // out points in file
+    Out_Point_Clusters_File(clusters, count_clasters); // out clusters in file
 }
 void main()
 {
